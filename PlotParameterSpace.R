5169754ad4dfe02da10c0de2d086944d303b333a
Scott Berry <scottdberry@gmail.com>
2015-03-18 13:55:37 +0000

    Changed Parameter Plot function

--- PlotParameterSpace.R
+++ PlotParameterSpace.R
@@ -9,25 +9,48 @@ four_color <- c(cbPalette[7],cbPalette[2],cbPalette[4],cbPalette[6])
 # Set the working directory
 setwd("~/local/Modelling/TwoStateCoupled/")
 
-file= "ParamOptimRes_s60r100000tr0_000st2.txt"
+file= "ParamOptimRes_s60r100000tr0_600st6.txt"
 parameterSpace <- read.table(file,header = TRUE)
 
 parameterSpace$firstPassageM[parameterSpace$firstPassageM == -1] <- max(parameterSpace$firstPassageM)
- 
+parameterSpace$firstPassageU[parameterSpace$firstPassageU == -1] <- max(parameterSpace$firstPassageU)
+
+## gap
+p <- ggplot(parameterSpace,aes(x=P_DEMETHYLATE,y=ENZYMATIC))
+p <- p + geom_tile(aes(fill=gap)) + scale_y_log10() + 
+  scale_fill_gradientn(colours=rev(rainbow(3))) + 
+  facet_grid(R_OFF ~ FIRING,labeller=label_both) + 
+  ggtitle("Average gap (P_OFF = 0.6)") + 
+  theme(plot.title = element_text(lineheight=.8, face="bold"),
+        panel.margin=unit(0,"lines")) + theme_bw(8)
+p
+
+## avgM
 p <- ggplot(parameterSpace,aes(x=P_DEMETHYLATE,y=ENZYMATIC))
 p <- p + geom_tile(aes(fill=Mavg)) + scale_y_log10() + 
   scale_fill_gradientn(colours=rev(rainbow(3)),limits=c(0,1)) + 
   facet_grid(R_OFF ~ FIRING,labeller=label_both) + 
-  ggtitle("Average M level over all loci (P_OFF = 0)") + 
+  ggtitle("Average M level over all loci (P_OFF = 0.6)") + 
   theme(plot.title = element_text(lineheight=.8, face="bold"),
-        panel.margin=unit(0,"lines")) + theme_bw()
+        panel.margin=unit(0,"lines")) + theme_bw(8)
 p
 
+## first passage M
 p <- ggplot(parameterSpace,aes(x=P_DEMETHYLATE,y=ENZYMATIC))
 p <- p + geom_tile(aes(fill=firstPassageM)) + scale_y_log10() + 
   scale_fill_gradientn(colours=rev(rainbow(3)),trans="log") + 
   facet_grid(R_OFF ~ FIRING,labeller=label_both) + 
-  ggtitle("First passage time (initial M) (P_OFF = 0)") + 
+  ggtitle("First passage time (initial M) (P_OFF = 0.6)") + 
   theme(plot.title = element_text(lineheight=.8, face="bold"),
-        panel.margin=unit(0,"lines")) + theme_bw()
+        panel.margin=unit(0,"lines")) + theme_bw(8)
+p
+
+## first passage U
+p <- ggplot(parameterSpace,aes(x=P_DEMETHYLATE,y=ENZYMATIC))
+p <- p + geom_tile(aes(fill=firstPassageU)) + scale_y_log10() + 
+  scale_fill_gradientn(colours=rev(rainbow(3)),trans="log") + 
+  facet_grid(R_OFF ~ FIRING,labeller=label_both) + 
+  ggtitle("First passage time (initial U) (P_OFF = 0.6)") + 
+  theme(plot.title = element_text(lineheight=.8, face="bold"),
+        panel.margin=unit(0,"lines")) + theme_bw(8)
 p
